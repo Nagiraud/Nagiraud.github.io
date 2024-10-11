cd4a18cd0f1fd71182384122e6e98bfdd54a0f66
Nagiraud <nathangiraud123@gmail.com>
2024-10-11 18:42:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,6 @@
 </head>
 <body>
   <p>Bjour</p></br>
-  <?php echo 'Bjour 2';
-  try{
-    $Po = "SELECT idR 
-           FROM rolesutilisateurprojet 
-           WHERE IdU = 2";
-  }
-  catch (PDOException $e){
-    echo 'Bjour 2784487747';
-  }?>
+  
 </body>
 </html>
